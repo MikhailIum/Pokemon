827bb84aa9fdb8badadbcd3c52b90150870a3c10
MikhailIum <73828917+MikhailIum@users.noreply.github.com>
2022-11-09 16:28:51 +0300

    Create Flamethrower.java

---
+++ src/moves/specialMoves/Flamethrower.java
@@ -0,0 +1,26 @@
+package ru.ifmo.pokemon.task;
+
+import ru.ifmo.se.pokemon.Effect;
+import ru.ifmo.se.pokemon.Pokemon;
+import ru.ifmo.se.pokemon.SpecialMove;
+import ru.ifmo.se.pokemon.Type;
+
+public class Flamethrower extends SpecialMove {
+
+    public Flamethrower(){
+        super(Type.FIRE, 90, 100);
+    }
+
+    @Override
+    protected String describe(){
+        return "кидает фаербол";
+    }
+
+    @Override
+    protected void applyOppEffects(Pokemon p){
+        double rand = Math.random();
+        if (rand <= 0.1){
+            Effect.burn(p);
+        }
+    }
+}
