5845806562aea5ba68bf62247a994ab320c6b0f8
MikhailIum <73828917+MikhailIum@users.noreply.github.com>
2022-11-09 16:30:08 +0300

    Create Liquidation.java

---
+++ src/moves/physicalMoves/Liquidation.java
@@ -0,0 +1,26 @@
+package ru.ifmo.pokemon.task;
+
+import ru.ifmo.se.pokemon.PhysicalMove;
+import ru.ifmo.se.pokemon.Pokemon;
+import ru.ifmo.se.pokemon.Stat;
+import ru.ifmo.se.pokemon.Type;
+
+public class Liquidation extends PhysicalMove {
+
+    public Liquidation(){
+        super(Type.WATER, 85, 100);
+    }
+
+    @Override
+    protected String describe(){
+        return "ликвидировал соперника";
+    }
+
+    @Override
+    protected void applyOppEffects(Pokemon p){
+        double rand = Math.random();
+        if (rand <= 0.2)
+            p.setMod(Stat.DEFENSE, -1);
+
+    }
+}
